4956e93655fe6182da109c9610d41ac944bbeca6
Adnan-5 <sahilladnan810@gmail.com>
2022-03-30 16:38:41 +0500

    get all the HTML Tags Using DOM

---
+++ app.js
@@ -0,0 +1,5 @@
+const button = document.querySelector("button");
+const cElement = document.getElementById("cElement");
+const sElement = document.getElementById("sElement");
+const speed = document.querySelector("input");
+const output = document.querySelector(".output");
